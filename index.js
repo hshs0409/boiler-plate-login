9439aa451e6796d9dae0ae578faca171f5cf873e
Lee Hyun Se <0409hshs@gmail.com>
2020-08-20 12:54:45 +0900

    Initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.get("/", (req, res) => res.send("Hi"));
+
+app.listen(PORT, () => console.log(`Server Connect Success in ${PORT} `));
